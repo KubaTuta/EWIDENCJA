1e61be6f6cdfd345e10c857893be3c3cf2251fb1
Kuba Tuta <tuta.jakub@gmail.com>
2025-10-20 21:02:39 +0200

    implementation JavaFX in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import javax.swing.*;
-import java.io.IOException;
+import javafx.application.Application;
 
 public class Main {
 
     public static void main(String[] args) {
-//
-        try {
-            App app = new App();
-            app.run();
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Application.launch(App.class, args);
     }
 }
